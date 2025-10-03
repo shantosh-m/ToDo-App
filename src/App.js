a8e171567e3df43ae5cb5a46d7ab8ef1ec47e68b
shantosh-m <e19371@eng.pdn.ac.lk>
2025-10-03 07:48:33 +0530

    Deletion, Addition function added
    Only 5 recent todos will be shown.
    When one todo deleted others will be shifted. Completed function is set as deleted. (Need to add some transition to be indicate that is completed)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import "./App.css";
+import { useState } from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { FaCheck } from "react-icons/fa";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const handleAdd = () => {
+    if (!title || !desc) return; // prevent empty todos
+    const newTodo = {
+      id: Date.now(),
+      title,
+      desc,
+    };
+    setTodos([newTodo, ...todos]); // add to top
+    setTitle("");
+    setDesc("");
+  };
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="App">
       <h1> My ToDos</h1>
@@ -11,7 +32,12 @@ function App() {
         <div className="todo-input">
           <div className="todo-input-item">
             <label>Title</label>
-            <input type="text" placeholder="What's the title of your To Do?" />
+            <input
+              type="text"
+              placeholder="What's the title of your To Do?"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
 
           <div className="todo-input-item">
@@ -19,28 +45,38 @@ function App() {
             <input
               type="text"
               placeholder="What's the description of your To Do?"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
             />
           </div>
 
           <div className="todo-input-item">
-            <button type="button" className="addBtn">
+            <button type="button" className="addBtn" onClick={handleAdd}>
               Add
             </button>
           </div>
         </div>
 
         <div className="todo-list">
-          <div className="todo-list-item">
-            <div>
-              <h3>Task 1</h3>
-              <p>Description</p>
-            </div>
+          {todos.slice(0, 5).map((todo) => (
+            <div key={todo.id} className="todo-list-item">
+              <div>
+                <h3>{todo.title}</h3>
+                <p>{todo.desc}</p>
+              </div>
 
-            <div>
-              <AiOutlineDelete className="icon" />
-              <FaCheck className="check-icon" />
+              <div>
+                <AiOutlineDelete
+                  className="icon"
+                  onClick={() => handleDelete(todo.id)}
+                />
+                <FaCheck
+                  className="check-icon"
+                  onClick={() => handleDelete(todo.id)}
+                />
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
